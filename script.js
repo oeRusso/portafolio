b404fe5a4c47c6fe7b27608ce37a93ed051367d6
Esteban russo <oerusso28@gmail.com>
2023-02-19 19:53:18 -0300

    Portafolio

---
+++ script.js
@@ -0,0 +1,17 @@
+// funcion q muestra el menu responsive
+
+function responsiveMenu(){
+
+    var x = document.getElementById("nav");
+
+    if (x.className === "") {
+        x.className = "responsive";
+    }else{
+        x.className = "";
+    }
+
+}
+
+
+
+/*TODO: min 59.00 */
